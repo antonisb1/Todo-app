9f51f9c9c666963e41c8d9b13c3715ded563dee5
antonisb1 <antonis.benardis@gmail.com>
2026-01-07 13:56:16 +0200

    d

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,7 @@
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL as string;
 const TOKEN_KEY = "token";
  
+console.log("API_BASE_URL:", API_BASE_URL);
 if (!API_BASE_URL) {
   throw new Error("VITE_API_BASE_URL is not set");
 }
